01e9e3973df7dbb1908485256c67eb7dbe2eb561
Henna <henna.haapala@gmail.com>
2024-01-17 21:15:35 +0200

    readme-päivitys ja jotain turhuuksia pois headerista

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # RYHMÄTYÖ WEB-KEHITYS M23A
 
-## Kuvat [unsplash](https://unsplash.com/) ja [flaticon](https://www.flaticon.com/) (todo tarkat krediittilinkit)
+## Mitä?
+Headerit ja footerit tulevat skriptinä jokaiselle sivulle, joten ne eivät näy sivut avaa paikallisesti.
+Analysoinnin kurssin jäljiltä koneella on todennäköisesti Python3 joten ajakaa kansiossa komentorivillä
+
+`python -m http.server`
+
+Jolloin tämä aukeaa `localhost:8000`-osoitteessa selaimessa. 
+
+#### TODO Henna:
+ - etusivun tuotekorttien muotoilut ja linkit tuotesivuille (tein siitä etusivusta nyt myös tuon shop-sivun samalla)
+ 
+ Yritin tuotesivun tuotekuvalla leikkiä silleen että se olisi aina saman kokoinen ja levynen näkymässä ilman että se kuva venyy ja paukkuu, eli siis `object-fit:cover;` samaan tyyliin kuin noissa etusivun korteissa mutta aivot loppu kesken. :D
+
+ ## Kuvat
+
+[unsplash](https://unsplash.com/) ja [flaticon](https://www.flaticon.com/)
--- script.js
+++ script.js
@@ -3,45 +3,22 @@ class SiteHeader extends HTMLElement {
     this.innerHTML = `
     <header class="container">
     <nav class="navbar d-flex justify-content-between align-items-center fs-5 py-1 container-fluid">
-    <!--ELEMENTS FOR MD AND UP-->
-        <div class="d-none d-sm-block">
+        <div class="">
           <a class="navbar-brand" href="index.html">
             <img src="img/keyboard_2.png" class="img-header-logo float-start" alt="site logo">
           </a>
         </div>
-        <div class="d-none d-sm-block">
+        <div class="">
           <ul class="nav">
             <li class="nav-item px-2 col-4"><a href="index.html" class="nav-link nav-color fw-bolder">Shop</a></li>
             <li class="nav-item px-2 col-4"><a href="about.html" class="nav-link nav-color fw-bolder">About</a></li>
             <li class="nav-item px-2 col-4"><a href="contact.html" class="nav-link nav-color fw-bolder">Contact</a></li>
           </ul>
         </div>
-        <div class="d-none d-sm-block">
+        <div class="">
           <span class="material-symbols-outlined px-2"><a href="#" class="nav-link">person</a></span>
           <span class="material-symbols-outlined px-2"><a href="#" class="nav-link">shopping_cart</a></span>
         </div>
-    <!--ELEMENTS FOR MD AND UP-->  
-    
-    <!--ELEMENTS FOR SMALLER SCREENS-->
-    <div class="navbar-header d-sm-none">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="material-symbols-outlined">menu</span>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse d-md-none" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-    
-    <!--ELEMENTS FOR SMALLER SCREENS-->
     </nav>
   </header>
   `
